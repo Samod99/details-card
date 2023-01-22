cc43af770273eba08ab41d5b20d6d9ff25fa6078
Binusha Samod <110772940+Samod99@users.noreply.github.com>
2023-01-22 19:25:26 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DetailsCard } from './models/details-card.model';
+import { DetailsCardService } from './service/details-card.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'details-card';
+  cards : DetailsCard[] = [];
+  card : DetailsCard = {
+    id : '',
+    cardholderName : '',
+    cardNumber : '',
+    cvc : '',
+    expiryMonth : '',
+    expiryYear : ''
+  }
+
+  constructor (private detailsCardService : DetailsCardService) {
+
+  }
+  ngOnInit(): void {
+    this.getAllCards();
+  }
+
+  getAllCards() {
+    this.detailsCardService.getAllCards()
+    .subscribe(
+      response => {
+        this.cards = response;
+      }
+    );
+  }
+
+  onSubmit() {
+    if (this.card.id === '') {
+      this.detailsCardService.addCard(this.card)
+      .subscribe(
+        response => {
+          this.getAllCards();
+          this.card = {
+            id : '',
+            cardholderName : '',
+            cardNumber : '',
+            cvc : '',
+            expiryMonth : '',
+            expiryYear : ''
+          }
+        }    
+      );
+    } else {
+      this.updateCard(this.card);
+    }  
+  }
+
+  deleteCard(id : string) {
+    this.detailsCardService.deleteCard(id)
+    .subscribe(
+      response => {
+        this.getAllCards();
+      }
+    );
+  }
+
+  populateForm(card : DetailsCard) {
+    this.card = card;
+  }
+
+  updateCard(card : DetailsCard) {
+    this.detailsCardService.updateCard(card)
+    .subscribe(
+      response => {
+        this.getAllCards();
+      }
+    );
+  }
 }
---
+++ src/app/service/details-card.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DetailsCard } from '../models/details-card.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DetailsCardService {
+
+  baseUrl =  '';
+
+  constructor(private http: HttpClient) { }
+
+  //Get All Cards
+  getAllCards() : Observable<DetailsCard[]> {
+    return this.http.get<DetailsCard[]>(this.baseUrl)
+  }
+
+  //Insert Cards
+  addCard(card : DetailsCard) : Observable<DetailsCard> {
+    card.id = '00000000-0000-0000-0000-000000000000';
+    return this.http.post<DetailsCard>(this.baseUrl, card);
+  }
+
+  //Delete Cards
+  deleteCard(id : string) : Observable<DetailsCard> {
+    return this.http.delete<DetailsCard>(this.baseUrl + '/' + id);
+  }
+
+  //Update Cards
+  updateCard(card : DetailsCard) : Observable<DetailsCard>{
+    return this.http.put<DetailsCard>(this.baseUrl + '/' + card.id, card);
+  }
+}
